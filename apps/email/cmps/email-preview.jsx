bde8404b82d147f1b9de372ddbd2106c6ccbe076
Itai Rotstein <itairotstein@gmail.com>
2022-04-27 17:07:28 +0300

    helpers font and color default

---
+++ apps/email/cmps/email-preview.jsx
@@ -0,0 +1,13 @@
+
+export function EmailPreview({ eMail }) {
+
+    const isRead = (eMail.isRead) ? 'isRead' : ''
+    return (
+        <article className={`email-preview flex space-between ${isRead}`}>
+            <span className="subject">{eMail.subject}</span>
+            <span className="body">{eMail.body}</span>
+            <span>{eMail.to}</span>
+            <span>{eMail.sentAt}</span>
+        </article>
+    )
+}
