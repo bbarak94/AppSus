61c5212a895c18b90c98f66370ab1c19bf1d2841
bbarak94 <bbarak94@gmail.com>
2022-04-28 12:45:40 +0300

    warming up

--- apps/keep/pages/keep-app.jsx
+++ apps/keep/pages/keep-app.jsx
@@ -2,6 +2,7 @@ import { KeepHeader } from '../cmps/keep-header.jsx'
 import { noteService } from '../services/note.service.js'
 import { NoteList } from '../cmps/note-list.jsx'
 import { NoteDetails } from '../cmps/note-details.jsx'
+import { NoteFilter } from '../cmps/note-filter.jsx'
 
 export class KeepApp extends React.Component {
     state = {
@@ -37,6 +38,30 @@ export class KeepApp extends React.Component {
             .then((selectedNote) => this.setState({ selectedNote }))
     }
 
+    onSetFilter = (filterBy) => {
+        this.setState({ filterBy }, () => {
+            console.log('filterBy from Keep App', this.state.filterBy)
+            this.loadNotes()
+        })
+    }
+
+    onRemoveNote = () => {
+        console.log('remove note:', this.state.selectedNote)
+        noteService.remove(this.state.selectedNote.id).then(() => {
+            this.loadNotes()
+            this.onSelectNote(null)
+        })
+    }
+
+    onAddNote = (ev) => {
+        console.log('ev.target.dataset.type:', ev.target.dataset.type)
+        var noteType = ev.target.dataset.type
+        noteService.createNote(noteType).then((newNoteId) => {
+            this.loadNotes()
+            this.onSelectNote(newNoteId)
+        })
+    }
+
     // onDeSelect = () => {
     //     this.setState({selectedNote: null})
     // }
@@ -47,9 +72,18 @@ export class KeepApp extends React.Component {
         return (
             <section className='keep-app'>
                 <KeepHeader />
-                <NoteList notes={notes} onSelectNote={this.onSelectNote} updateNote={this.updateNote} />
+                <NoteFilter
+                    onAddNote={this.onAddNote}
+                    onSetFilter={this.onSetFilter}
+                />
+                <NoteList
+                    notes={notes}
+                    onSelectNote={this.onSelectNote}
+                    updateNote={this.updateNote}
+                />
                 {selectedNote && (
                     <NoteDetails
+                        onRemoveNote={this.onRemoveNote}
                         updateNote={this.updateNote}
                         note={selectedNote}
                     />
