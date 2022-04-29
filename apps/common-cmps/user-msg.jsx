6f1c68111ad54ecf8b8265b0da7db1b411348bad
Itai Rotstein <itairotstein@gmail.com>
2022-04-29 10:31:02 +0300

    firday start

---
+++ apps/common-cmps/user-msg.jsx
@@ -0,0 +1,43 @@
+import { eventBusService } from "../../services/event-bus-service.js"
+const { Link } = ReactRouterDOM
+
+export class UserMsg extends React.Component {
+
+    state = {
+        msg: null
+    }
+
+    removeEvent;
+    timeoutId;
+
+    componentDidMount() {
+        this.removeEvent = eventBusService.on('user-msg', (msg) => {
+            console.log('msg form event listener', msg)
+            this.setState({ msg })
+            if (this.timeoutId) clearTimeout(this.timeoutId)
+            this.timeoutId = setTimeout(this.onCloseMsg, 3000)
+        })
+    }
+
+    onCloseMsg = () => {
+        this.setState({ msg: null })
+        clearTimeout(this.timeoutId)
+    }
+
+    componentWillUnmount() {
+        this.removeEvent()
+    }
+
+
+
+    render() {
+        const { msg } = this.state
+        if (!msg) return <React.Fragment></React.Fragment>
+        return (
+            <div className={`user-msg ${msg.type}`}>
+                    <button onClick={this.onCloseMsg}>X</button>
+                    {msg.txt}
+            </div>
+        )
+    }
+}
