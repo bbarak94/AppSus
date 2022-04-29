d2a42c2e9357090ecf97c361e0eb336ff5ca3210
Itai Rotstein <itairotstein@gmail.com>
2022-04-29 16:31:20 +0300

    friday noon

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -1,10 +1,36 @@
 const { Link } = ReactRouterDOM
 
 export class EmailPreview extends React.Component {
+    state = {
+        windowWidth: window.innerWidth
+    }
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({ windowWidth: window.innerWidth })
+        })
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', () => {
+            this.setState({ windowWidth: window.innerWidth })
+        })
+    }
 
     bodyLong(body) {
-        if (body.length > 50) return body.slice(0, 50) + '...'
-        return body
+        const { windowWidth } = this.state
+        if (body.length > 70 && windowWidth > 1500) return body.slice(0, 90) + '...'
+        else if (windowWidth > 1300) return body.slice(0, 55) + '...'
+        else if (windowWidth > 1150) return body.slice(0, 30) + '...'
+        else if (windowWidth > 980) return body.slice(0, 15) + '...'
+        return body.slice(0, 7) + '...'
+    }
+
+    subjectLong(subject) {
+        const { windowWidth } = this.state
+        if (subject.length > 20 && windowWidth > 1500) return subject.slice(0, 20) + '...'
+        else if (windowWidth > 1300) return subject.slice(0, 10) + '...'
+        return subject.slice(0, 5) + '...'
+        
     }
 
     render() {
@@ -14,14 +40,14 @@ export class EmailPreview extends React.Component {
 
         return (
             <article className={`email-preview  ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
-                <div className="img-container" onClick={() => onSetIsStarred(eMail.id)}><img src={`assets\\img\\mail\\${isStarredClass}.png`} /></div>
+                <img src={`assets\\img\\mail\\${isStarredClass}.png`} onClick={() => onSetIsStarred(eMail.id)} />
                 <Link to={`/email/${eMail.id}`}>
                     <table>
                         <tbody>
                             <tr>
-                                <td className={`preview-subject${isReadClass}`}>{eMail.subject}</td>
+                                <td className={`preview-to ${isReadClass}`}>{eMail.to}</td>
+                                <td className={`preview-subject ${isReadClass}`}>{this.subjectLong(eMail.subject)}</td>
                                 <td className="preview-body">{this.bodyLong(eMail.body)}</td>
-                                <td className="preview-to">{eMail.to}</td>
                                 <td className="sent-at">{eMail.sentAt}</td>
                             </tr>
                         </tbody>
