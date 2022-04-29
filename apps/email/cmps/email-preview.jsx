77f880b435da13dfae3236e18d87074c4e2cf93b
Itai Rotstein <itairotstein@gmail.com>
2022-04-29 12:52:59 +0300

    kkk

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -2,7 +2,10 @@ const { Link } = ReactRouterDOM
 
 export class EmailPreview extends React.Component {
 
-
+    bodyLong(body) {
+        if (body.length > 50) return body.slice(0, 50) + '...'
+        return body
+    }
 
     render() {
         const { eMail, onSetIsRead, onSetIsStarred } = this.props
@@ -10,14 +13,14 @@ export class EmailPreview extends React.Component {
         const isStarredClass = (eMail.isStarred) ? 'starred' : 'un-starred'
 
         return (
-            <article className={`email-preview flex  ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
-                <img src={`assets\\img\\mail\\${isStarredClass}.png`} onClick={() => onSetIsStarred(eMail.id)}/>
+            <article className={`email-preview  ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
+                <div className="img-container" onClick={() => onSetIsStarred(eMail.id)}><img src={`assets\\img\\mail\\${isStarredClass}.png`} /></div>
                 <Link to={`/email/${eMail.id}`}>
                     <table>
                         <tbody>
                             <tr>
                                 <td className={`preview-subject${isReadClass}`}>{eMail.subject}</td>
-                                <td className="preview-bodybody">{eMail.body}</td>
+                                <td className="preview-body">{this.bodyLong(eMail.body)}</td>
                                 <td className="preview-to">{eMail.to}</td>
                                 <td className="sent-at">{eMail.sentAt}</td>
                             </tr>
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -3,6 +3,7 @@ import { EmailApp } from './apps/email/pages/email-app.jsx'
 import { KeepApp } from './apps/keep/pages/keep-app.jsx'
 import { EmailDetails } from './apps/email/pages/email-details.jsx'
 import { UserMsg } from './apps/common-cmps/user-msg.jsx'
+import { About } from './apps/common-cmps/about.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
@@ -15,6 +16,7 @@ export function App() {
                <Route path='/email/:emailId' component={EmailDetails} />
                <Route path='/email' component={EmailApp} />
                <Route path='/keep' component={KeepApp} />
+               <Route path='/about' component={About} />
                <Route path='/' component={AppHome} />
             </Switch>
          </section>
