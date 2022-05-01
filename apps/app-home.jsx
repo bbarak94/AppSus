04570246f97162b7b74f4d24e290fe72d5974a56
bbarak94 <bbarak94@gmail.com>
2022-05-02 00:43:48 +0300

    00:45

--- apps/app-home.jsx
+++ apps/app-home.jsx
@@ -1,18 +1,25 @@
-import { eventBusService } from "../services/event-bus-service.js"
+import { eventBusService } from '../services/event-bus-service.js'
 
 export class AppHome extends React.Component {
-
-
     componentDidMount() {
         eventBusService.emit('selectedPage', 'home')
     }
 
     render() {
-
         return (
-            <section className='app-home'>
-                <div className="hero">
-                    <h1>Wellcome To Barai</h1>
+            <section className='app-home flex'>
+                <div className='home-page-txt'>
+                <h1 className='line-one'>Prepare to increase</h1>
+                <h1 className='line-two'>Your productivity</h1>
+                </div>
+
+
+
+                <div className='home-img-container'>
+                    <img
+                        className='home-img'
+                        src='assets\img\common-imgs\home-img.jpg'
+                    ></img>
                 </div>
             </section>
         )
