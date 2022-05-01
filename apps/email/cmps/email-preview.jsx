236cfd12c9d00bb2c87b6b3b84bd23d91308f4af
Itai Rotstein <itairotstein@gmail.com>
2022-05-01 20:59:41 +0300

    blat

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -16,19 +16,21 @@ export class EmailPreview extends React.Component {
     }
 
     bodyLong(body) {
-        const { windowWidth } = this.state
-        if (windowWidth > 1500) return body.slice(0, 70) + '...'
-        else if (windowWidth > 1300) return body.slice(0, 60) + '...'
-        else if (windowWidth > 1000) return body.slice(0, 30) + '...'
-        else if (windowWidth > 720) return body.slice(0, 0) + '...'
-        else return body.slice(0, 60) + '...'
+        // const { windowWidth } = this.state
+        // if (windowWidth > 1500) return body.slice(0, 70) + '...'
+        // else if (windowWidth > 1300) return body.slice(0, 60) + '...'
+        // else if (windowWidth > 1000) return body.slice(0, 30) + '...'
+        // else if (windowWidth > 720) return body.slice(0, 0) + '...'
+        // else return body.slice(0, 70) + '...'
+        return body.slice(0, 70)
     }
 
     subjectLong(subject) {
-        const { windowWidth } = this.state
-        if (windowWidth > 1500) return subject.slice(0, 25)
-        else if (windowWidth > 1300) return subject.slice(0, 15)
-        return subject.slice(0, 5) + '...'
+        // const { windowWidth } = this.state
+        // if (windowWidth > 1500) return subject.slice(0, 25)
+        // else if (windowWidth > 1300) return subject.slice(0, 15)
+        // else if (windowWidth < 720 )return subject.slice(0, 30) + '...'
+        return subject.slice(0, 20)
     }
 
     render() {
@@ -37,18 +39,18 @@ export class EmailPreview extends React.Component {
         const isStarredClass = (eMail.isStarred) ? 'starred' : 'un-starred'
 
         return (
-            <article className={`email-preview  ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
-                <img src={`assets/img/mail/${isStarredClass}.png`} onClick={() => onSetIsStarred(eMail.id)} />
-                <Link to={`/email/${eMail.id}`}>
-                    <section className="preview-container">
-                        {(filterBy.status === 'inbox') && <div className={`preview-from ${isReadClass}`}>{eMail.from}</div>}
-                        {(filterBy.status === 'sent') && <div className={`preview-to ${isReadClass}`}>{eMail.to}</div>}
-                        <div className={`preview-subject ${isReadClass}`}>{this.subjectLong(eMail.subject)}</div>
-                        <div className="preview-body">{this.bodyLong(eMail.body)}</div>
-                        <div className="sent-at">{eMail.sentAt}</div>
-                    </section>
-                </Link>
-            </article>
+            // <article className={`email-preview ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
+            <Link to={`/email/${eMail.id}`}>
+                <section className={`preview-container ${isReadClass}`} onClick={() => onSetIsRead(eMail.id)}>
+                    <div className="star"><img src={`assets/img/mail/${isStarredClass}.png`} onClick={() => onSetIsStarred(eMail.id)} /></div>
+                    {(filterBy.status === 'inbox' || filterBy.status === 'starred') && <div className={`preview-from ${isReadClass}`}>{eMail.from}</div>}
+                    {(filterBy.status === 'sent') && <div className={`preview-to ${isReadClass}`}>{eMail.to}</div>}
+                    <div className={`preview-subject ${isReadClass}`}>{this.subjectLong(eMail.subject)}</div>
+                    <div className="preview-body">{this.bodyLong(eMail.body)}</div>
+                    <div className="sent-at"><pre>         {eMail.sentAt}</pre></div>
+                </section>
+            </Link>
+            // </article>
         )
     }
 }
